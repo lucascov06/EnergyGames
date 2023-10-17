21200a21268e91e20e056590aff82b987c0ce2ba
lucascov06 <lgscovasso@gmail.com>
2023-10-16 23:01:43 -0300

    redirigiendo a cada pagina los botones

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 const playButton = document.getElementById("play-button");
 const creditsButton = document.getElementById("credits-button");
 
+playButton.addEventListener("click", () => {
+  // Redirigir al usuario a la página del juego al hacer clic en "Play"
+  window.location.href = "raw.html";
+});
+
+creditsButton.addEventListener("click", () => {
+  // Redirigir al usuario a la página de créditos al hacer clic en "Créditos"
+  window.location.href = "creditos.html";
+});
