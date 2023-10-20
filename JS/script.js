5eb2f16610833c3a2ffc25906f3c480acae99aea
obertti.thiago.epet20@gmail.com <Obertti.thiago.epet20@gmail.com>
2023-10-20 15:35:46 -0300

    hover buttons

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,18 @@ creditsButton.addEventListener("click", () => {
   // Redirigir al usuario a la página de créditos al hacer clic en "Créditos"
   window.location.href = "creditos.html";
 });
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  var navmenu__menu = document.querySelector(".nav-menu__menu");
+  var toTop = document.querySelector(".to-top");
+
+  header.classList.toggle("down", window.scrollY > 0);
+  navmenu__menu.classList.toggle("down", window.scrollY > 0);
+
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
