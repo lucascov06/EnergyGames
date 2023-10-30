9bde1e1236dcaaadff5c4322ad08c0998031699a
lucascov06 <scovasso.luca.epet20@gmail.com>
2023-10-29 23:48:52 -0300

    generando el personaje

--- JS/raw.js
+++ JS/raw.js
@@ -2,3 +2,15 @@
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
+const character = {
+    x: 0,
+    y: canvas.height + 420,
+    width: 30,
+    height: 30,
+    speed: 5,
+    jumpHeight: 150,
+    isJumping: false,
+    isAttacking: false,
+};
+
+const initialCharacter = { ...character };
